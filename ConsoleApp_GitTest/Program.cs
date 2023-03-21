524785389020f9efbb632ba574e3e4dd16ebbc5e
Jinge <jinge_windows@hotmail.com>
2023-03-22 10:57:47 +1100

    revert

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -6,4 +6,5 @@ Console.WriteLine("Revert the change that [Rename the Student class to Dog]");
 Console.WriteLine("Create a new branch.");
 Console.WriteLine("Commit No_1 in branch_1.");
 Console.WriteLine("Commit No_2 in branch_2, create teacher class.");
+Console.WriteLine("Revert");
 Console.ReadKey();
