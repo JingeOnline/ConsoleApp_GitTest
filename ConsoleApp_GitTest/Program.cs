b741d1baa3607ad417ff55bee415d5a62d911e6b
Jinge <jinge_windows@hotmail.com>
2023-03-22 09:58:28 +1100

    Commit No_3, create student.cs

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Commit No_2")
+Console.WriteLine("Commit No_2");
+Console.WriteLine("Commit No_3");
 Console.ReadKey();
