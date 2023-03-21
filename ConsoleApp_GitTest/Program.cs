dca5deaad84fc5bc925ab253ba7d6bea112eef02
Jinge <jinge_windows@hotmail.com>
2023-03-22 10:33:28 +1100

    After rebase, commit No_3 in branch_1, the class doesn't have subject class.

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -6,4 +6,5 @@ Console.WriteLine("Revert the change that [Rename the Student class to Dog]");
 Console.WriteLine("Create a new branch.");
 Console.WriteLine("Commit No_1 in branch_1.");
 Console.WriteLine("Commit No_2 in branch_2, create teacher class.");
+Console.WriteLine("After rebase, commit No_3 in branch_1, the class doesn't have subject class.");
 Console.ReadKey();
