cd27d56d24537cec1f4fa57fc501ecccc2f9c37b
Jinge <jinge_windows@hotmail.com>
2023-03-22 10:06:19 +1100

    Revert the change that [Rename the Student class to Dog]

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -2,4 +2,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Commit No_2");
 Console.WriteLine("Commit No_3");
+Console.WriteLine("Revert the change that [Rename the Student class to Dog]");
 Console.ReadKey();
