0f1ab4fe8a691f40f1fba7613395f29797fe62f5
Jinge <jinge_windows@hotmail.com>
2023-03-22 10:17:04 +1100

    Commit No_2 in branch_2, create teacher class.

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -5,4 +5,5 @@ Console.WriteLine("Commit No_3");
 Console.WriteLine("Revert the change that [Rename the Student class to Dog]");
 Console.WriteLine("Create a new branch.");
 Console.WriteLine("Commit No_1 in branch_1.");
+Console.WriteLine("Commit No_2 in branch_2, create teacher class.");
 Console.ReadKey();
