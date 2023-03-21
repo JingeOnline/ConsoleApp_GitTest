54c082db53ac426e75ed081a237af0ad5f116e53
Jinge <jinge_windows@hotmail.com>
2023-03-22 10:13:44 +1100

    Commit No_1 in branch_1

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -4,4 +4,5 @@ Console.WriteLine("Commit No_2");
 Console.WriteLine("Commit No_3");
 Console.WriteLine("Revert the change that [Rename the Student class to Dog]");
 Console.WriteLine("Create a new branch.");
+Console.WriteLine("Commit No_1 in branch_1.");
 Console.ReadKey();
