a3665ef0a2d55eadda0cc5d8307fe66c1868b4ce
Jinge <jinge_windows@hotmail.com>
2023-03-22 10:30:30 +1100

    Create new class Subject

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -6,4 +6,6 @@ Console.WriteLine("Revert the change that [Rename the Student class to Dog]");
 Console.WriteLine("Create a new branch.");
 Console.WriteLine("Commit No_1 in branch_1.");
 Console.WriteLine("Commit No_2 in branch_2, create teacher class.");
+Console.WriteLine( "Merge from branch_1 to master");
+Console.WriteLine("Create new class Subject");
 Console.ReadKey();
