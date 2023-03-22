cd46caeef357f6cadc24e8cb007f0a710617b70b
Jinge <jinge_windows@hotmail.com>
2023-03-22 11:01:44 +1100

    Reveret to commit No_1

--- ConsoleApp_GitTest/Program.cs
+++ ConsoleApp_GitTest/Program.cs
@@ -1,12 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Commit No_2");
-Console.WriteLine("Commit No_3");
-Console.WriteLine("Revert the change that [Rename the Student class to Dog]");
-Console.WriteLine("Create a new branch.");
-Console.WriteLine("Commit No_1 in branch_1.");
-Console.WriteLine("Commit No_2 in branch_2, create teacher class.");
-Console.WriteLine( "Merge from branch_1 to master");
-Console.WriteLine("Create new class Subject");
-Console.WriteLine("Revert");
+Console.WriteLine("Comment No_1");
 Console.ReadKey();
